b2aeec820ae49bf25f9b7d87d3f77230481ae49f
navepno <navepno@gmail.com>
2022-08-30 11:21:07 +0300

    delete all

--- README.md
+++
@@ -1,1 +0,0 @@
-cool bot for telegram
--- acfa.py
+++
@@ -1,9 +0,0 @@
-# placeholders = ', '.join('?' * 3)
-# placeholders.replace('?', '%s')
-# print(placeholders)
-
-placeholders = ''
-for i in range(3):
-    placeholders += '%s, '
-placeholders = placeholders[:len(placeholders) - 2]
-print(placeholders)
